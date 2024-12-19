7eb388969477145fb1f09f4bf191dbe8c59abd91
David Fung <davidfung@amgcomputing.com>
2024-12-19 09:25:49 -0800

    Next 14.5.3 Constant instructions

--- chunk.go
+++ chunk.go
@@ -5,17 +5,25 @@ const (
 )
 
 type Chunk struct {
-	code []uint8
+	code      []uint8
+	constants ValueArray
 }
 
 func initChunk(chunk *Chunk) {
 	chunk.code = nil
+	initValueArray(chunk.constants)
 }
 
 func writeChunk(chunk *Chunk, code uint8) {
 	chunk.code = append(chunk.code, code)
 }
 
+func addConstant(chunk *Chunk, value Value) int {
+	writeValueArray(chunk.constants, value)
+	return len(chunk.constants.values) - 1
+}
+
 func freeChunk(chunk *Chunk) {
+	freeValueArrary(chunk.constants)
 	initChunk(chunk)
 }
--- value.go
+++ value.go
@@ -1,3 +1,19 @@
 package main
 
 type Value float64
+
+type ValueArray struct {
+	values []Value
+}
+
+func initValueArray(array ValueArray) {
+	array.values = nil
+}
+
+func writeValueArray(array ValueArray, value Value) {
+	array.values = append(array.values, value)
+}
+
+func freeValueArrary(array ValueArray) {
+	initValueArray(array)
+}
