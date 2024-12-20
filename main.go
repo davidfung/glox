550ded70a51838df93e223672af867508b0b2d05
David Fung <davidfung@amgcomputing.com>
2024-12-20 09:24:59 -0800

    next 15.1.1

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 func main() {
+	initVM()
+
 	var chunk Chunk
 	initChunk(&chunk)
 
@@ -11,5 +13,6 @@ func main() {
 	writeChunk(&chunk, OP_RETURN, 123) //TODO can we just pass chunk instead of &chunk?
 
 	disassembleChunk(&chunk, "test chunk")
+	freeVM()
 	freeChunk(&chunk)
 }
