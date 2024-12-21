88bc160f397b6970feae930c8c0963f4e44f4711
David Fung <davidfung@amgcomputing.com>
2024-12-21 15:53:50 -0800

    next implement run()

--- vm.go
+++ vm.go
@@ -2,14 +2,25 @@ package main
 
 type VM struct {
 	chunk *Chunk
+	ip    *uint8
 }
+
+const (
+	INTERPRET_OK = iota
+	INTERPRET_COMPILE_ERROR
+	INTERPRET_RUNTIME_ERROR
+)
 
 var vm VM
 
 func initVM() {
-
 }
 
 func freeVM() {
+}
 
+func interpret(chunk *Chunk) int {
+	vm.chunk = chunk
+	vm.ip = &vm.chunk.code[0]
+	return INTERPRET_OK //TOFIX run()
 }
