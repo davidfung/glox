5bcccc2f3a6e3d214ed6e64aa655c5388c657a82
David Fung <davidfung@amgcomputing.com>
2025-01-03 22:17:47 -0800

    taking command line arguments

--- main.go
+++ main.go
@@ -1,36 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"os"
+)
+
 const DEBUG_TRACE_EXECUTION = true
+
+func repl() {
+}
+
+func runFile(path string) {
+}
 
 func main() {
 	initVM()
 
-	var chunk Chunk
-	initChunk(&chunk)
+	if len(os.Args) == 1 {
+		repl()
+	} else if len(os.Args) == 2 {
+		runFile(os.Args[1])
+	} else {
+		fmt.Fprintln(os.Stderr, "Usage: glox [path]")
+	}
 
-	var constant int
-
-	constant = addConstant(&chunk, 1.2)
-	writeChunk(&chunk, OP_CONSTANT, 123)
-	writeChunk(&chunk, uint8(constant), 123)
-
-	constant = addConstant(&chunk, 3.4)
-	writeChunk(&chunk, OP_CONSTANT, 123)
-	writeChunk(&chunk, uint8(constant), 123)
-
-	writeChunk(&chunk, OP_ADD, 123)
-
-	constant = addConstant(&chunk, 5.6)
-	writeChunk(&chunk, OP_CONSTANT, 123)
-	writeChunk(&chunk, uint8(constant), 123)
-
-	writeChunk(&chunk, OP_DIVIDE, 123)
-	writeChunk(&chunk, OP_NEGATE, 123)
-
-	writeChunk(&chunk, OP_RETURN, 123) //TODO can we just pass chunk instead of &chunk?
-
-	disassembleChunk(&chunk, "test chunk")
-	interpret(&chunk)
 	freeVM()
-	freeChunk(&chunk)
 }
