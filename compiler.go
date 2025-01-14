db69e98dbb6762cb4194c2eca0717e34e4e13c46
David Fung <davidfung@amgcomputing.com>
2025-01-14 09:37:03 -0800

    next scanToken()

--- compiler.go
+++ compiler.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 type Scanner struct {
 	start   int
 	current int
@@ -16,4 +18,19 @@ func initScanner(source string) {
 
 func compile(source string) {
 	initScanner(source)
+	line := -1
+	for {
+		token := scanToken()
+		if token.line != line {
+			fmt.Printf("%4d ", token.line)
+			line = token.line
+		} else {
+			fmt.Printf("   | ")
+		}
+		fmt.Printf("%2d '%.*s'\n", token.type, token.length, token.start) //TOFIX
+
+		if token.type == TOKEN_EOF {
+			break
+		}
+	}
 }
